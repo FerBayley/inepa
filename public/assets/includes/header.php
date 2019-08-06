30fa46702908080adc634a85118fd774f94b9205
ferbayley <fernando.bayley@gmail.com>
2019-08-06 18:14:45 -0300

    primer commit

---
+++ public/assets/includes/header.php
@@ -0,0 +1,44 @@
+    <!-- Header -->
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="assets/images/logo.png" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="servicios.php">Servicios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="psicoterapia.php">Psicoterapia</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cursos.php">Cursos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="nosotros.php">Nosotros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contacto.php">Contacto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fab fa-facebook"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fab fa-whatsapp-square"></i></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    </header>
+    <!-- /Header -->
